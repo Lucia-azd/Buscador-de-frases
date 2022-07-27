49adcc18ee148c7b6f7094a397fdbd5847221cca
Lucia Agha Zadeh Dehdeh <azd.lucia@gmail.com>
2022-07-26 21:13:49 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char* argv[] ){
     if(cantidad_argumentos < 3){
         cout<< "\n" << "Cantidad de argumentos invalidos, por favor ingrese dos argumentos - 'Historia' y 'Frase a buscar' -" << "\n" <<endl;
     }else{
+        cout<< "\n" << "Historia: " << argv[1] << "\n" <<endl;
+        cout<< "\n" << "Frase a buscar: " << argv[2] << "\n" <<endl;
         cargar_vector(argv[1], argv[2]);
     }
     return 0;
